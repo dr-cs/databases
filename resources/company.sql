c8ef34fa8145a81a234760bc4fe631e36b712338
Chris Simpkins <chris.simpkins@gmail.com>
2016-03-17 02:29:27 -0400

    Add create table statements to company.sql

--- resources/company.sql
+++ resources/company.sql
@@ -1,3 +1,63 @@
+create table employee (
+  Fname varchar(15) not null,
+  Minit char,
+  Lname varchar(15) not null,
+  Ssn char(9) not null,
+  Bdate date,
+  Address varchar(30),
+  Sex char,
+  Salary decimal(10,2),
+  Super_ssn char(9),
+  Dno int,
+  primary key (Ssn)
+);
+
+create table department (
+  Dname varchar(15) not null,
+  Dnumber int not null,
+  Mgr_ssn char(9) not null,
+  Mgr_start_date date,
+  primary key (Dnumber),
+  unique (Dname),
+  foreign key (Mgr_ssn) references employee (Ssn)
+);
+
+create table dept_locations (
+  Dnumber int not null,
+  Dlocation varchar(15) not null,
+  primary key (Dnumber, Dlocation),
+  foreign key (Dnumber) references department (Dnumber)
+);
+
+create table project (
+  Pname varchar(15) not null,
+  Pnumber int not null,
+  Plocation varchar(15),
+  Dnum int not null,
+  primary key (Pnumber),
+  unique (Pname),
+  foreign key (Dnum) references department (Dnumber)
+);
+
+create table works_on (
+  Essn char(9) not null,
+  Pno int not null,
+  Hours decimal(3,1) not null,
+  primary key (Essn, Pno),
+  foreign key (Essn) references employee (Ssn),
+  foreign key (Pno) references project (Pnumber)
+);
+
+create table dependent (
+  Essn char(9) not null,
+  Dependent_name varchar(15) not null,
+  Sex char,
+  Bdate date,
+  Relationship varchar(8),
+  primary key (Essn, Dependent_name),
+  foreign key (Essn) references employee(Ssn)
+);
+
 insert into EMPLOYEE values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731 Fondren, Houston, TX', 'M', 30000, 333445555, 5);
 insert into EMPLOYEE values ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638 Voss, Houston, TX', 'M', 40000, 888665555, 5);
 insert into EMPLOYEE values ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321 Castle, Spring, Tx', 'F', 25000, 987654321, 4);
